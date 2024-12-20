cffae01d0c467c6211a34f3a0141b817a22124b5
SanekWhite <bro.b98@mail.ru>
2024-12-20 12:31:22 +0700

    Initial Commit

---
+++ ToDoNotesStorage/ToDoNotesStorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoNotesStorageApp.swift
+//  ToDoNotesStorage
+//
+//  Created by Александр Белый on 20.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoNotesStorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
